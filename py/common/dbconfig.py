65a67c2d936a74d8d10a37bf44506e1c54af4872
Stefan Vitanov <fractalexperience@mail.bg>
2024-03-18 00:56:22 +0200

    dbconfig.py

--- py/common/dbconfig.py
+++ py/common/dbconfig.py
@@ -4,23 +4,23 @@
 # DB_USER = 'sv7203_rdf'
 # DB_PASS = 'Poi09Uyt87'
 
-
-# Pythonanywhere database - remote
-DB_HOST = 'localhost'
-DB_NAME = 'sv7203$rdf'
-DB_USER = 'sv7203'
-DB_PASS = 'Poi09Uyt87'
-SSH_HOST = 'ssh.pythonanywhere.com'
-SSH_USER = 'sv7203'
-SSH_PASS = 'XQV.f_JR{vb?}'
-SSH_BIND_ADDRESS = 'sv7203.mysql.pythonanywhere-services.com'
-
-# # Pythonanywhere database - local
-# DB_HOST = 'sv7203.mysql.pythonanywhere-services.com'
+#
+# # Pythonanywhere database - remote
+# DB_HOST = 'localhost'
 # DB_NAME = 'sv7203$rdf'
 # DB_USER = 'sv7203'
 # DB_PASS = 'Poi09Uyt87'
-# SSH_HOST = None
-# SSH_USER = None
-# SSH_PASS = None
-# SSH_BIND_ADDRESS = None
+# SSH_HOST = 'ssh.pythonanywhere.com'
+# SSH_USER = 'sv7203'
+# SSH_PASS = 'XQV.f_JR{vb?}'
+# SSH_BIND_ADDRESS = 'sv7203.mysql.pythonanywhere-services.com'
+
+# Pythonanywhere database - local
+DB_HOST = 'sv7203.mysql.pythonanywhere-services.com'
+DB_NAME = 'sv7203$rdf'
+DB_USER = 'sv7203'
+DB_PASS = 'Poi09Uyt87'
+SSH_HOST = None
+SSH_USER = None
+SSH_PASS = None
+SSH_BIND_ADDRESS = None
