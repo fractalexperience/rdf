c40f1bbae07967a2a7e0642aedc152174c972ed8
Stefan Vitanov <fractalexperience@mail.bg>
2024-07-08 21:27:19 +0300

    dbconfig.py

--- py/common/dbconfig.py
+++ py/common/dbconfig.py
@@ -1,12 +1,12 @@
-# Local rdf
-DB_HOST = 'localhost'
-DB_NAME = 'rdf'
-DB_USER = 'root'
-DB_PASS = 'Poi09Uyt87'
-SSH_HOST = None
-SSH_USER = None
-SSH_PASS = None
-SSH_BIND_ADDRESS = None
+# # Local rdf
+# DB_HOST = 'localhost'
+# DB_NAME = 'rdf'
+# DB_USER = 'root'
+# DB_PASS = 'Poi09Uyt87'
+# SSH_HOST = None
+# SSH_USER = None
+# SSH_PASS = None
+# SSH_BIND_ADDRESS = None
 
 # # mocha database
 # DB_HOST = '192.250.227.28'
@@ -24,12 +24,12 @@ SSH_BIND_ADDRESS = None
 # SSH_PASS = 'XQV.f_JR{vb?}'
 # SSH_BIND_ADDRESS = 'sv7203.mysql.pythonanywhere-services.com'
 
-# # Pythonanywhere database - local
-# DB_HOST = 'sv7203.mysql.pythonanywhere-services.com'
-# DB_NAME = 'sv7203$rdf'
-# DB_USER = 'sv7203'
-# DB_PASS = 'Poi09Uyt87'
-# SSH_HOST = None
-# SSH_USER = None
-# SSH_PASS = None
-# SSH_BIND_ADDRESS = None
+# Pythonanywhere database - local
+DB_HOST = 'sv7203.mysql.pythonanywhere-services.com'
+DB_NAME = 'sv7203$rdf'
+DB_USER = 'sv7203'
+DB_PASS = 'Poi09Uyt87'
+SSH_HOST = None
+SSH_USER = None
+SSH_PASS = None
+SSH_BIND_ADDRESS = None
